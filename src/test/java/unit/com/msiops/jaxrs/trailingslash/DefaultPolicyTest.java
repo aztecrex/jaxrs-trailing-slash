784b12888dcc0fd3b5d51b536582b0f022305715
Greg Wiley <aztec.rex@jammm.com>
2014-06-14 11:47:32 -0700

    [tooling] prepare to add functional tests

--- src/test/java/unit/com/msiops/jaxrs/trailingslash/DefaultPolicyTest.java
+++ src/test/java/unit/com/msiops/jaxrs/trailingslash/DefaultPolicyTest.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package test.com.msiops.jaxrs.trailingslash;
+package unit.com.msiops.jaxrs.trailingslash;
 
 import static org.junit.Assert.*;
 
